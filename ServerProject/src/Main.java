53e6fb9120ef234f5d58991ef0455a0f5fabfc1c
JRivera340 <you@example.com>
2025-07-31 15:08:25 -0500

    prueba

--- ServerProject/src/Main.java
+++ ServerProject/src/Main.java
@@ -1,55 +1,108 @@
 import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        //192.168.131.37
-
-
+    public void  init() throws IOException {
         var server = new ServerSocket(8080);
 
         var isAlive = true;
 
         while(isAlive){
+
             System.out.println("Servidor Escuchando");
 
             var socket = server.accept();
 
             System.out.println("Cliente Conectado");
 
+            dispatchWorcker(socket);
+
+        }
+    }
+
+    private void dispatchWorcker(Socket socket) {
+        new Thread(
+                ()->{
+                    try {
+                        handleRequest(socket);
+
+
+                    } catch (IOException e){
+                        throw new RuntimeException();
+                    }
+
+                }
+        ).start();
+    }
+
+    private void handleRequest(Socket socket) throws IOException {
+
+
+        var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String line;
+        while ((line = reader.readLine()) != null && !line.isEmpty()){
+
+            if (line.startsWith("GET")) {
+                var resource = line.split(" ")[1].replace("/"," ");
+                System.out.println("El cliente esta pidiendo "+ resource);
+
+                //Enviar la response
+
+                sendResponse(socket);
+            }
+
+        }
+
+    }
+
+    private void sendResponse(Socket socket) throws IOException {
+
+        var file = new File("");
+        System.out.println(file.getAbsolutePath());
+        var writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        //Copy
+        var response = "<html><body>Hola Mundo</body></html>";
+        writer.write("HTTP/1.0 200 OK\r\n");
+        writer.write("Content-Type: text/html\r\n");
+        writer.write("Content-Length: 34\r\n");
+        writer.write("Connection: close\r\n");
+        writer.write("\r\n");
+        writer.write(response);
+
+        writer.close();
+        socket.close();
+        //Paste
+    }
+
+    public static void main(String[] args) throws IOException {
+        //192.168.131.37
+
+        Main main = new Main();
+        main.init();
+
+        /*
+        var isAlive = true;
+
+        while(isAlive){
+
             //Respuesta
 
             var os = socket.getOutputStream();
             var in = socket.getInputStream();
 
-            var writer = new BufferedWriter(new OutputStreamWriter(os));
 
-            var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null && !line.isEmpty()){
-                System.out.println(line);
-
-            }
-
-            writer.write("HTTP/1.0 200 OK\r\n");
-            writer.write("Content-Type: text/html\r\n");
-            writer.write("Content-Length: 34\r\n");
-            writer.write("Connection: close\r\n");
-            writer.write("\r\n");
-            writer.write("<html><body>Hola Mundo</body></html>");
-
-            writer.flush();
-
-            writer.close();
-            socket.close();
         }
         server.close();
 
+
+         */
+
     }
 
-    //Terminar servidor multihilos para el jueves, implementar hilos.
+
 }

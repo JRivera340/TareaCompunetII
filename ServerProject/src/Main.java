2904788492b45c9392cd06d30237a177eaf3bb0e
JRivera340 <you@example.com>
2025-07-31 15:32:30 -0500

    Avance con archivos de prueba .html

--- ServerProject/src/Main.java
+++ ServerProject/src/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
 
                     } catch (IOException e){
-                        throw new RuntimeException();
+                        e.printStackTrace();
                     }
 
                 }
@@ -48,35 +48,52 @@ public class Main {
         while ((line = reader.readLine()) != null && !line.isEmpty()){
 
             if (line.startsWith("GET")) {
-                var resource = line.split(" ")[1].replace("/"," ");
+                var resource = line.split(" ")[1].replace("/","");
                 System.out.println("El cliente esta pidiendo "+ resource);
 
                 //Enviar la response
 
-                sendResponse(socket);
+                sendResponse(socket, resource);
             }
 
         }
 
     }
 
-    private void sendResponse(Socket socket) throws IOException {
+    private void sendResponse(Socket socket, String resources) throws IOException {
 
         var file = new File("");
         System.out.println(file.getAbsolutePath());
-        var writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        //Copy
-        var response = "<html><body>Hola Mundo</body></html>";
-        writer.write("HTTP/1.0 200 OK\r\n");
-        writer.write("Content-Type: text/html\r\n");
-        writer.write("Content-Length: 34\r\n");
-        writer.write("Connection: close\r\n");
-        writer.write("\r\n");
-        writer.write(response);
+        var res = new File("resources/"+resources);
+        System.out.println(res.getAbsolutePath());
 
-        writer.close();
-        socket.close();
-        //Paste
+        if (res.exists()) {
+            var fis = new FileInputStream(res);
+            var br = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            StringBuilder response = new StringBuilder();
+            while((line = br.readLine()) != null){
+                response.append(line);
+            }
+
+            var writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            //Copy
+
+            writer.write("HTTP/1.0 200 OK\r\n");
+            writer.write("Content-Type: text/html\r\n");
+            writer.write("Content-Length: "+response.length()+"\r\n");
+            writer.write("Connection: close\r\n");
+            writer.write("\r\n");
+            writer.write(response.toString());
+
+            writer.close();
+            socket.close();
+            //Paste
+        } else {
+            System.out.println("No se encuentra el archivo");
+        }
+
+
     }
 
     public static void main(String[] args) throws IOException {
